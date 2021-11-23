b29be1f428df22b53f203ac2fd47ad7a45058d3e
BrunoHibou <brunocavalcanticosta.bc@gmail.com>
2021-11-23 02:27:31 -0300

    adition and subtraction
    adição e subtração kinda funcionam, preciso arrumar multiplicação e divisão

--- transformations/Transform.py
+++ transformations/Transform.py
@@ -1,21 +1,21 @@
-import numpy.matrixlib as np
+import numpy as np
 import PIL.Image as pimg
 
 
 # módulo referente às transformações
 class PI:
 
-    def image_to_matrix(img):
+    def image_to_matrix(self, img):
         image = pimg.open(img).convert("L")
         image_array = np.matrix(image)
+        image_array = np.squeeze(image_array)
         return image_array
 
     # função que realiza a adição entre uma imagem A e uma imagem B
     def addition(self, image1, image2):
         result = image1
-        result.fill(0)
-        for index, value in enumerate(image1):
-            for index2, value2 in enumerate(image1[index]):
+        for index in range(len(image1)):
+            for index2 in range(len(image1[index])):
                 result[index][index2] = image1[index][index2] + image2[index][index2]
 
         return result
@@ -23,9 +23,8 @@ class PI:
     # função que realiza a subtração entre uma imagem A e uma imagem B
     def subtraction(self, image1, image2):
         result = image1
-        result.fill(0)
-        for index, value in enumerate(image1):
-            for index2, value2 in enumerate(image1[index]):
+        for index in range(len(image1)):
+            for index2 in range(len(image1[index])):
                 result[index][index2] = image1[index][index2] - image2[index][index2]
 
         return result
@@ -33,19 +32,32 @@ class PI:
     # função que realiza a multiplicação entre uma imagem A e uma imagem B
     def multiplication(self, image1, image2):
         result = image1
-        result.fill(0)
-        for index, value in enumerate(image1):
-            for index2, value2 in enumerate(image1[index]):
-                result[index][index2] = image1[index][index2] - image2[index][index2]
+        for index in range(len(image1)):
+            for index2 in range(len(image1[index])):
+                result[index][index2] = (image1[index][index2]) * (image2[index][index2])
 
         return result
 
     # função que realiza a divisão entre uma imagem A e uma imagem B
     def division(self, image1, image2):
         result = image1
-        result.fill(0)
-        for index, value in enumerate(image1):
-            for index2, value2 in enumerate(image1[index]):
-                result[index][index2] = image1[index][index2] - image2[index][index2]
+        for index in range(len(image1)):
+            for index2 in range(len(image1[index])):
+                result[index][index2] = image1[index][index2] / image2[index][index2]
 
         return result
+
+
+if __name__ == "__main__":
+    pi = PI()
+
+    image1 = "../imagens/lena.png"
+    image2 = "../imagens/lenainverted.png"
+
+    image1 = pi.image_to_matrix(image1)
+
+    image2 = pi.image_to_matrix(image2)
+
+    image3 = pimg.fromarray(pi.multiplication(image1, image2))
+    image3.show()
+
